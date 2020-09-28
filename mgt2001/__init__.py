123382839520e22aa05f82a452ac1bc4d78a56dd
Brian L. Chen <brian.lxchen@gmail.com>
2020-09-28 21:22:22 +0800

    Add original autolabel

--- mgt2001/__init__.py
+++ mgt2001/__init__.py
@@ -5,6 +5,17 @@ import pandas as pd
 import numpy as np
 from adjustText import adjust_text
 
+def ori_autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        plt.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
 def autolabel(rects, **kwargs):
     """
     Attach a text label above each bar in *rects*, displaying its height.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mgt2001",
-    version="0.0.4",
+    version="0.0.5",
     author="Brian L. Chen",
     author_email="brian.lxchen@gmail.com",
     description="A small package for MGT 2001 use only",
