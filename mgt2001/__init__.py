9da7ff91bdd02595db52803f6d39b9771cdafd65
Brian L. Chen <brian.lxchen@gmail.com>
2020-10-01 21:08:37 +0800

    Add margin_setter

--- mgt2001/__init__.py
+++ mgt2001/__init__.py
@@ -5,15 +5,16 @@ import pandas as pd
 import numpy as np
 from adjustText import adjust_text
 
+
 def ori_autolabel(rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
         height = rect.get_height()
         plt.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+                     xy=(rect.get_x() + rect.get_width() / 2, height),
+                     xytext=(0, 3),  # 3 points vertical offset
+                     textcoords="offset points",
+                     ha='center', va='bottom')
 
 
 def autolabel(rects, **kwargs):
@@ -29,10 +30,9 @@ def autolabel(rects, **kwargs):
     for rect in rects:
         height = rect.get_height()
         texts.append(plt.text(s='{}'.format(height),
-                    x=(rect.get_x() + rect.get_width() / 2),
-                    y=(height)))
+                              x=(rect.get_x() + rect.get_width() / 2),
+                              y=(height)))
 
-    
     if "arrowprops" in kwargs:
         adjust_text(texts, arrowprops=kwargs['arrowprops'])
 
@@ -44,7 +44,30 @@ def color_palette(n, cmap="jet"):
     '''
 
     get_color = getattr(cm, cmap)
-    return get_color(np.linspace(0,1,n))
+    return get_color(np.linspace(0, 1, n))
+
+
+def add_margin(ax, x=0.05, y=0.05):
+    '''
+     Setting margins in matplotlib/seaborn with subplots.
+
+     This will, by default, add 5% to the x and y margins. You
+     can customize this using the x and y arguments when you call it.
+     ref: https://stackoverflow.com/a/34205235/10871988
+
+     e.g.
+     ax = sns.regplot(x='Temperature', y= 'Tickets', data = df, ci = None, scatter_kws={'color':'dodgerblue'}, line_kws={'linestyle':'dashed', 'color':'#ffaa77'})
+     add_margin(ax, x=0.01, y=0.00) ### Call this after regplot 
+    '''
+
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    xmargin = (xlim[1]-xlim[0])*x
+    ymargin = (ylim[1]-ylim[0])*y
+
+    ax.set_xlim(xlim[0]-xmargin, xlim[1]+xmargin)
+    ax.set_ylim(ylim[0]-ymargin, ylim[1]+ymargin)
 
 
 class Pos():
@@ -59,7 +82,8 @@ class Pos():
     def __calcPos(self):
         self.pos = list()
         for i in range(self.seriesN):
-            self.pos.append([j - (1 - self.space) / 2. + i * self.width for j in range(1,self.groupedN+1)])
+            self.pos.append([j - (1 - self.space) / 2. + i *
+                             self.width for j in range(1, self.groupedN+1)])
 
     def returnPos(self):
         return self.pos
