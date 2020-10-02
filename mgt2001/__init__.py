435c676ca3d77d60216a6faf6c8509f8267cadbf
Brian L. Chen <brian.lxchen@gmail.com>
2020-10-02 10:22:01 +0800

    Add truncate arg in autolabel

--- mgt2001/__init__.py
+++ mgt2001/__init__.py
@@ -6,11 +6,11 @@ import numpy as np
 from adjustText import adjust_text
 
 
-def ori_autolabel(rects):
+def ori_autolabel(rects, truncate='{}'):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
         height = rect.get_height()
-        plt.annotate('{}'.format(height),
+        plt.annotate(truncate.format(height),
                      xy=(rect.get_x() + rect.get_width() / 2, height),
                      xytext=(0, 3),  # 3 points vertical offset
                      textcoords="offset points",
@@ -23,15 +23,24 @@ def autolabel(rects, **kwargs):
     Only *arrowprops* is available right now.
     """
     if "original" in kwargs and kwargs["original"] == True:
-        ori_autolabel(rects)
+        if "truncate" in kwargs:
+            ori_autolabel(rects, kwargs["truncate"])
+            return
+        else:
+            ori_autolabel(rects)
         return
 
     texts = []
     for rect in rects:
         height = rect.get_height()
-        texts.append(plt.text(s='{}'.format(height),
-                              x=(rect.get_x() + rect.get_width() / 2),
-                              y=(height)))
+        if "truncate" in kwargs:
+            texts.append(plt.text(s=kwargs["truncate"].format(height),
+                                  x=(rect.get_x() + rect.get_width() / 2),
+                                  y=(height)))
+        else:
+            texts.append(plt.text(s='{}'.format(height),
+                                  x=(rect.get_x() + rect.get_width() / 2),
+                                  y=(height)))
 
     if "arrowprops" in kwargs:
         adjust_text(texts, arrowprops=kwargs['arrowprops'])
