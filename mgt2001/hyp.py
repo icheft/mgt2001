374927e590c3e3f7477fa7f3b5bd80d81efe0717
Brian L. Chen <brian.lxchen@gmail.com>
2020-12-17 08:04:08 +0800

    Add power_test function

--- mgt2001/hyp.py
+++ mgt2001/hyp.py
@@ -4,7 +4,69 @@ import math
 import scipy.stats as stats
 
 
-def rejection_region_method(x_mean, mu, std, n, alpha, option='left', precision=4):
+def power_test(x_mean, h0_mean, std, n, alpha, h1_mean, option='left', precision=4, show=True, ignore=True):
+    opt = option.lower()[0]
+    if opt == 't':
+        option = 'Two-Tail Test'
+        x_l, x_u = rejection_region_method(
+            x_mean, h0_mean, std, n, alpha, option=opt, precision=precision, show=show, ignore=ignore)
+        z_value = stats.norm.ppf(1 - alpha / 2)
+        z_l = -z_value
+        z_u = z_value
+        z_type2_l = (x_l - h1_mean) / (std / (n ** 0.5))
+        z_type2_u = (x_u - h1_mean) / (std / (n ** 0.5))
+        type2_p_l = stats.norm.cdf(z_type2_l)
+        type2_p_u = stats.norm.cdf(z_type2_u)
+        type2_p = type2_p_u - type2_p_l
+        ptest = 1 - type2_p
+        result = f'''======= Evaluating Type II Errors ({option}) =======
+μ = {h1_mean}
+z (lower bound) = {z_type2_l:.{precision}f}
+z (upper bound) = {z_type2_u:.{precision}f}
+
+z_l (Lower bound for the critical value) = {z_l:.{precision}f}
+z_u (Upper bound for the critical value) = {z_u:.{precision}f}
+
+x_l (Lower bound for x critical value) = {x_l:.{precision}f}
+x_u (Upper bound for x critical value) = {x_u:.{precision}f}
+
+P(Type II Error) = {type2_p:.{precision}f}
+Power of a Test = {ptest:.{precision}f}
+        '''
+    else:
+        x_c = rejection_region_method(
+            x_mean, h0_mean, std, n, alpha, option=opt, precision=precision, show=show, ignore=ignore)
+        if opt == 'l':
+            option = 'One-Tail Test (left tail)'
+            z_c = -stats.norm.ppf(1 - alpha)
+            z_type2 = (x_c - h1_mean) / (std / (n ** 0.5))
+            type2_p = 1 - stats.norm.cdf(z_type2)
+            ptest = 1 - type2_p
+
+        elif opt == 'r':
+            option = 'One-Tail Test (right tail)'
+            z_c = stats.norm.ppf(1 - alpha)
+            z_type2 = (x_c - h1_mean) / (std / (n ** 0.5))
+            type2_p = stats.norm.cdf(z_type2)
+            ptest = 1 - type2_p
+
+        result = f'''======= Evaluating Type II Errors ({option}) =======
+μ = {h1_mean}
+z = {z_type2:.{precision}f}
+
+z critical value = {z_c:.{precision}f}
+x critical value = {x_c:.{precision}f}
+
+P(Type II Error) = {type2_p:.{precision}f}
+Power of a Test = {ptest:.{precision}f}
+'''
+
+    print(result)
+
+    return type2_p, ptest
+
+
+def rejection_region_method(x_mean, mu, std, n, alpha, option='left', precision=4, show=True, ignore=False):
     opt = option.lower()[0]
     if opt == 't':
         option = 'Two-Tail Test'
@@ -12,8 +74,8 @@ def rejection_region_method(x_mean, mu, std, n, alpha, option='left', precision=
         x_u = mu + z_value * std / math.sqrt(n)
         x_l = mu - z_value * std / math.sqrt(n)
         flag = x_mean < x_l or x_mean > x_u
-
-        result = f'''======= The Rejection Region Method =======
+        if not ignore:
+            result = f'''======= The Rejection Region Method =======
 Significant Level (alpha) = {alpha:.{precision}f}
 z_value = {z_value:.{precision}f}
 
@@ -22,7 +84,17 @@ x̄ =  {x_mean:.{precision}f}
 x_l (Lower bound for the critical value) = {x_l:.{precision}f}
 x_u (Upper bound for the critical value) = {x_u:.{precision}f}
 Reject H_0 → {flag}
-        '''
+            '''
+        else:
+            result = f'''======= The Rejection Region Method =======
+Significant Level (alpha) = {alpha:.{precision}f}
+z_value = {z_value:.{precision}f}
+
+Using {option}:
+x_l (Lower bound for the critical value) = {x_l:.{precision}f}
+x_u (Upper bound for the critical value) = {x_u:.{precision}f}
+            '''
+
     else:
         if opt == 'l':
             # left tail
@@ -35,8 +107,8 @@ Reject H_0 → {flag}
             z_value = stats.norm.ppf(1 - alpha)
             x_c = mu + z_value * std / math.sqrt(n)
             flag = x_mean > x_c
-
-        result = f'''======= The Rejection Region Method =======
+        if not ignore:
+            result = f'''======= The Rejection Region Method =======
 Significant Level (alpha) = {alpha:.{precision}f}
 z_value = {z_value:.{precision}f}
 
@@ -44,16 +116,26 @@ Using {option}:
 x̄ =  {x_mean:.{precision}f}
 x_c (Critical value) = {x_c:.{precision}f}
 Reject H_0 → {flag}
-        '''
+            '''
+        else:
+            result = f'''======= The Rejection Region Method =======
+Significant Level (alpha) = {alpha:.{precision}f}
+z_value = {z_value:.{precision}f}
 
-    print(result)
+Using {option}:
+x_c (Critical value) = {x_c:.{precision}f}
+            '''
+
+    if show:
+        print(result)
+
     if opt == 't':
         return x_l, x_u
     else:
         return x_c
 
 
-def testing_statistic_method(x_mean, mu, std, n, alpha, option='left', precision=4):
+def testing_statistic_method(x_mean, mu, std, n, alpha, option='left', precision=4, ignore=False):
     opt = option.lower()[0]
     z = (x_mean - mu)/(std / math.sqrt(n))
     if opt == 't':
@@ -63,7 +145,8 @@ def testing_statistic_method(x_mean, mu, std, n, alpha, option='left', precision
         z_l = -z_value
         flag = z <= z_l or z >= z_u
 
-        result = f'''======= Testing Statistic Method =======
+        if not ignore:
+            result = f'''======= Testing Statistic Method =======
 Significant Level (alpha) = {alpha:.{precision}f}
 z_value = {z_value:.{precision}f}
 
@@ -72,7 +155,17 @@ z =  {z:.{precision}f}
 z_l (Lower bound for the critical value) = {z_l:.{precision}f}
 z_u (Upper bound for the critical value) = {z_u:.{precision}f}
 Reject H_0 → {flag}
-        '''
+            '''
+        else:
+            result = f'''======= Testing Statistic Method =======
+Significant Level (alpha) = {alpha:.{precision}f}
+z_value = {z_value:.{precision}f}
+
+Using {option}:
+z_l (Lower bound for the critical value) = {z_l:.{precision}f}
+z_u (Upper bound for the critical value) = {z_u:.{precision}f}
+            '''
+
     else:
         if opt == 'l':
             # left tail
@@ -84,7 +177,8 @@ Reject H_0 → {flag}
             z_value = stats.norm.ppf(1 - alpha)
             flag = z >= z_value
 
-        result = f'''======= Testing Statistic Method =======
+        if not ignore:
+            result = f'''======= Testing Statistic Method =======
 Significant Level (alpha) = {alpha:.{precision}f}
 z_value = {z_value:.{precision}f}
 
@@ -92,7 +186,16 @@ Using {option}:
 z =  {z:.{precision}f}
 z_alpha (Critical value) = {z_value:.{precision}f}
 Reject H_0 → {flag}
-        '''
+            '''
+
+        else:
+            result = f'''======= Testing Statistic Method =======
+Significant Level (alpha) = {alpha:.{precision}f}
+z_value = {z_value:.{precision}f}
+
+Using {option}:
+z_alpha (Critical value) = {z_value:.{precision}f}
+            '''
 
     print(result)
     if opt == 't':
@@ -102,6 +205,7 @@ Reject H_0 → {flag}
 
 
 def inter_p_value(p_value):
+    # interpretation
     if p_value >= 0 and p_value < 0.01:
         inter_p = 'Overwhelming Evidence'
     elif p_value >= 0.01 and p_value < 0.05:
@@ -122,7 +226,7 @@ def p_value_method(x_mean, h0_mean, h0_std, samp_num, siglevel, option='left', p
         option = 'Two-Tail Test'
         p_value = (1 - stats.norm.cdf(z_value)) * 2
         zcv = stats.norm.ppf(1 - siglevel/2)
-        flag = p_value <= alpha * 2
+        flag = p_value <= alpha
         sub_result = f'''Using {option}:
 Difference = {x_mean - h0_mean}
 z (Critical value) = {-zcv:.{precision}f}, {zcv:.{precision}f}
